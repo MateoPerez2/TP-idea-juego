15ebfccc6ab30c3c1d1f872dcd947999dedf9a29
MateoPerez2 <mateopaparella@hotmail.com>
2022-06-01 10:43:35 -0300

    jump with space and reset player position

--- Assets/colisionCapsula.cs
+++ Assets/colisionCapsula.cs
@@ -8,11 +8,14 @@ public class colisionCapsula : MonoBehaviour
     public float rotationSpeed;
     public GameObject barrera;
     public float velocidadBarrera;
+    Rigidbody rb;
+    public float force;
+    bool hasJump = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -52,13 +55,23 @@ public class colisionCapsula : MonoBehaviour
         {
             barrera.transform.Translate(0 ,-velocidadBarrera , 0);
         }
+        if(Input.GetKeyDown(KeyCode.Space) && hasJump)
+        {
+            rb.AddForce(Vector3.up * force, ForceMode.Impulse);
+            hasJump = false;
+        }
     }
 
     void OnCollisionEnter(Collision col)
     {
         if (col.gameObject.name == "Deathwall")
         {
-            Destroy(gameObject);
+            transform.position = new Vector3(6, 1, 0);
+            barrera.transform.position = new Vector3(26, 5, -1.19f);
+        }
+        if (col.gameObject.tag == "ground")
+        {
+            hasJump = true;
         }
     }
 }
