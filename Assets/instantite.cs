36a597c21e93ff1f7ef8af33ff4d99491532f202
MateoPerez2 <mateopaparella@hotmail.com>
2022-06-07 15:59:31 -0300

    for + instantiate

---
+++ Assets/instantite.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class instantite : MonoBehaviour
+{
+    public float speed;
+    public GameObject prefab1;
+    GameObject clone;
+    float posX = -329.9511f;
+    float posY = 11.02f;
+    float posZ = -4.28f;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        for(int i=0; i < 5; i++)
+        {
+            
+            clone = Instantiate(prefab1);
+            clone.transform.position = new Vector3(posX, posY, posZ);
+            posZ++;
+            Destroy(clone, 1);
+            
+        }
+        for(int i=5; i > 0; i--)
+        {
+            clone = Instantiate(prefab1);
+            clone.transform.position = new Vector3(posX, posY, posZ);
+            posZ --;
+            Destroy(clone, 1);
+            
+        }
+    }
+}
